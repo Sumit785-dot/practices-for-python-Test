9fd67539fd7cd050a33c176f89e94766827c96b4
Sumit785-dot <sumitmewada736@gmail.com>
2025-12-19 23:04:43 +0530

    Update main.py from CodeMonitor

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+# Basic Functions
 
-print("Ji")
+# Q1: Ek simple function fun() banao jo "Welcome to GFG" print kare .
+
+def fun():
+    print("welcome to gfc")
+fun()
+# Q2: Ek function evenOdd(x) likho jo check kare ki number even hai ya odd aur result print kare .
+
+def evenOdd(x):
+    if x%2==0:
+        print("even")
+    else:
+        print("odd")
+evenOdd(22)
+
+# Q3: Ek function greet(name) banao jo input name ke saath "Hello" print kare .
+
+def greet(name):
+    print(f"{name} hello")
+greet("sumir")
