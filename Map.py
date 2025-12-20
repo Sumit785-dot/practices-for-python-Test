6a0516754d78b7c9f8a04247901c031524eae7a8
Sumit785-dot <sumitmewada736@gmail.com>
2025-12-20 10:51:44 +0530

    Update Map.py from CodeMonitor

---
+++ Map.py
@@ -0,0 +1,6 @@
+# Q11 (Map): map() aur lambda use karke ek list [1, 2, 3, 4, 5] ke saare numbers ka square nikalo .
+
+
+lis = [1,2,3,4,5,6,7,8,9,10]
+result = list(map(lambda x:x*2,lis))
+print(result)
