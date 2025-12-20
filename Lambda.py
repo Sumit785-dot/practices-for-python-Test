58432dba65b7556be4df9de47a50860909e36bea
Sumit785-dot <sumitmewada736@gmail.com>
2025-12-20 10:46:17 +0530

    Update Lambda.py from CodeMonitor

---
+++ Lambda.py
@@ -0,0 +1,14 @@
+# Lambda, Map, Filter, Reduce
+# Q10 (Lambda):
+
+# Ek lambda function banao jo number ko double (x*2) kare.
+dou = lambda a:a*2
+
+print(dou(2))
+# Ek lambda function banao jo number ka cube (x^3) nikale.
+dou = lambda a:a*3
+
+print(dou(2))
+# Ek lambda function banao jo 3 numbers ka average nikale.
+avg = lambda a,b,c:a+b+c/(3)
+print(avg(12,23,24))
